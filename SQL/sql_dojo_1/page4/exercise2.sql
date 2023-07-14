bc0d27030220149f1749bd4a0ad0a6a367a64d26
Muhammad Fatih Yumna Lajuwirdi Lirrahman <m.fatihyumna25@gmail.com>
2023-07-14 19:05:09 +0800

    dojo I + sertif 💈

---
+++ SQL/sql_dojo_1/page4/exercise2.sql
@@ -0,0 +1,4 @@
+-- dapatkan laba rata-rata dari semua produk
+SELECT  AVG(price - cost)
+FROM items
+;
---
+++ SQL/sql_dojo_1/page5/exercise1.sql
@@ -0,0 +1,5 @@
+-- dapatkan semua produk yang harganya lebih tinggi dari harga "hoodie abu-abu"
+SELECT name, price
+FROM items
+WHERE price >= 60
+;
