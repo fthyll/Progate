735332c989ba1f2b44fe6965d10ce9f40eb86c4a
Muhammad Fatih Yumna Lajuwirdi Lirrahman <m.fatihyumna25@gmail.com>
2023-07-20 23:34:41 +0800

    JavaScript V 💛

---
+++ JavaScript/es6_study_5/page3/script.js
@@ -0,0 +1,11 @@
+// Hapus code di bawah
+/*import Dog from "./dog";
+
+const dog = new Dog("Leo", 4, "Chihuahua");
+*/
+// Hapus code di atas
+
+// Import constant dog
+import dog from "./dogData";
+
+dog.info();
