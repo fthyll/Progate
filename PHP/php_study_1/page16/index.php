e709428cf3265c19f5739ac1ac04cb5952ca0117
Muhammad Fatih Yumna Lajuwirdi Lirrahman <m.fatihyumna25@gmail.com>
2023-08-01 07:42:50 +0800

    PHP I 🟣

---
+++ PHP/php_study_1/page16/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    $scores = array('Matematika' => 70, 'Bahasa' => 90, 'Sains' => 80);
+
+    // Tulis loop foreach dibawah
+    foreach ($scores as $key => $value) {
+      echo $key.' : '.$value;
+      echo '<br>';
+    }
+    
+  ?>
+
+</body>
+</html>
